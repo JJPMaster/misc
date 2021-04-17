527f292abb96826abea7d78a649cef6da1eb65c9
JJPMaster <78509994+JJPMaster@users.noreply.github.com>
2021-04-16 22:06:03 -0400

    Create alert.js

---
+++ alert.js
@@ -0,0 +1,13 @@
+/* This script will automatically display a random JS alert message anytime you click on something. */
+
+function makeid(length) {
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+   
+}
+alert(makeid(15));
